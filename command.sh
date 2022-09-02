c0f8432e964a26a0d0ddeca4e50d12b638496237
Pratik Mishra <93151291+theredcosmos@users.noreply.github.com>
2022-09-03 00:36:42 +0530

    Create command.sh

---
+++ command.sh
@@ -0,0 +1,15 @@
+Git commands used 
+
+git remote add origin https://github.com/theredcosmos/propeller_blackbird.git
+git branch -M main
+git push -u origin main
+
+git brach update_logo
+git checkout update_logo
+git add *
+git commit -m "changed logo and updated link"
+
+git checkout main
+git pull
+git merge update_logo
+git push origin main
